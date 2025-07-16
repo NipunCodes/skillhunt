21c577ed48e07560aa0bf9b3532ba2a4c9ea2cd8
Pasindu Nipun <nipunrjay@gmail.com>
2025-07-16 16:07:53 +0530

    Added Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div className='text-[50px]'>Hello MFs!</div>
+  return (  
+    <div>
+      <Footer />
+    </div>
+  )  
 }
 
 export default App
