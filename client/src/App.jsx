39943967eccd6d593fbb9c3550b3cfea4dde5bd4
Pasindu Nipun <nipunrjay@gmail.com>
2025-07-17 01:21:44 +0530

    Implemented Router for Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx' 
+import About from './pages/About.jsx'
+import ClientDashboard from './pages/ClientDashboard.jsx'
+import NoPage from './pages/NoPage.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Footer />
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/clientdashboard" element={<ClientDashboard />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
     )
 }
