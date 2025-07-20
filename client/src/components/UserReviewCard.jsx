0966334ed166fe1ea35f1ea84be32f995d26a6bf
LahiruCodez <gglahirusanjaya@gmail.com>
2025-07-20 18:37:00 +0530

    Home Page Done

---
+++ client/src/components/UserReviewCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DP from '../assets/profile-pic.jpg'
+
+const UserReviewCard = () => {
+
+  return (
+      <div className='bg-gray-200 border border-gray-400 shadow-[5px_5px_5px_hsla(244,100%,10%,0.493)] p-10 max-w-70 text-center rounded-xl m-10 inline-block'>
+        <img src={DP} alt="profile-pic" className='rounded-full' />
+        <h1 className="text-3xl font-semibold text-[rgb(0,50,150)] font-sans mt-5">USER</h1>
+        <p className='mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas error, nobis rem doloremque illum praesentium nesciunt perferendis quos voluptatem dolorum?</p>
+      </div>
+  )
+}
+
+export default UserReviewCard
