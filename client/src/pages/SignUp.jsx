d9314a1263b5a64361fccf802a766f314375c45f
LahiruCodez <gglahirusanjaya@gmail.com>
2025-07-26 18:53:23 +0530

    Job create form page done.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -18,34 +18,34 @@ function SignUp() {
               <h1 className='text-white text-center text-4xl font-bold font-mono mb-5'>SIGN UP</h1>
                 <div className='flex justify-center p-5 sm:flex-row flex-col sm:space-x-6 space-y-4 sm:space-y-0'>
                   <div className='flex flex-col flex-grow-1'>
-                    <label className='text-white text-xl font-semibold mb-3 font-sans' htmlFor="firstname">First Name</label>
-                    <input className='bg-white border border-gray-300 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname" />
+                    <label className='text-white text-xl font-semibold font-sans' htmlFor="firstname">First Name</label>
+                    <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname" />
                   </div>
                   <div className='flex flex-col flex-grow-1'>
-                    <label className='text-white text-xl font-semibold mb-3 font-sans' htmlFor="firstname">Last Name</label>
-                    <input className='bg-white border border-gray-300 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname" />
+                    <label className='text-white text-xl font-semibold font-sans' htmlFor="firstname">Last Name</label>
+                    <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname" />
                   </div>
                 </div>
               <div className='justify-center p-5 font-sans sm:flex-row flex-col sm:space-x-6 space-y-4 sm:space-y-0'>
                 <label className='text-white text-xl font-semibold' htmlFor="email">Email</label>
-                <input className='bg-white border border-gray-300 mt-0.5 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="email" name="email" id="email" required/>
+                <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="email" name="email" id="email" required/>
               </div>
               <div className='justify-center p-5 font-sans sm:flex-row flex-col sm:space-x-6 space-y-4 sm:space-y-0'>
                 <label className='text-white text-xl font-semibold' htmlFor="password">Password</label>
-                <input className='bg-white border border-gray-300 mt-0.5 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300 ' type="password" required/>
+                <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300 ' type="password" required/>
               </div>
               <div className='justify-center p-5 font-sans sm:flex-row flex-col sm:space-x-6 space-y-4 sm:space-y-0'>
-                <label className='text-white text-xl font-semibold mb-3 font-sans' htmlFor="username">User Name</label>
-                <input className='bg-white border border-gray-300 mt-0.5 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="username" id="username" />
+                <label className='text-white text-xl font-semibold font-sans' htmlFor="username">User Name</label>
+                <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="username" id="username" />
               </div>
               <div className='flex gap-6 justify-center p-5 text-xl font-semibold  font-sans sm:flex-row flex-col sm:space-x-6 space-y-4 sm:space-y-0'>
                 <div className='flex flex-col'>
                 <label className='text-white' htmlFor="gender">Gender</label>
-                  <input className='bg-white border border-gray-300 mt-0.5 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname"   />
+                  <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="firstname" id="firstname"   />
                 </div>
                 <div className='flex flex-col'>
                   <label className='text-white' htmlFor="calendar">Calendar</label>
-                  <input className='bg-white border border-gray-300 mt-0.5 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="lastname" id="lastname"   />
+                  <input className='bg-white border border-gray-300 mt-1 h-10 rounded-xl pr-10 w-full focus:outline-none focus:ring-1 focus:ring-blue-300' type="text" name="lastname" id="lastname"   />
                 </div>
               </div>
               <div className='flex justify-center items-center p-5'>
