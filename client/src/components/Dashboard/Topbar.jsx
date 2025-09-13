6fd58bf6209cea72c72a66df2f5155e6f954f78a
Pasindu Nipun <nipunrjay@gmail.com>
2025-09-13 15:38:13 +0530

    Update Topbar.jsx

--- client/src/components/Dashboard/Topbar.jsx
+++ client/src/components/Dashboard/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsBell } from 'react-icons/bs'
 
 const Topbar = () => {
   return (
@@ -9,6 +10,16 @@ const Topbar = () => {
                 <span className='text-xs block text-stone-500'>
                     Thursday, Sep 4th 2025
                 </span>
+            </div>
+
+            {/* Notification Badge */}
+            <div className='relative'>
+                <button className='p-2 hover:bg-stone-100 rounded-lg transition-colors duration-200 relative'>
+                    <BsBell className='text-xl text-stone-600 hover:text-stone-800' />
+                    <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center'>
+                        3
+                    </span>
+                </button>
             </div>
         </div>
     </div>
